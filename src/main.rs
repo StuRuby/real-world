986a37f35188511d00d7564f2a1880ded594c003
zhongwu <xingya.sxy@alibaba-inc.com>
2022-02-10 00:09:48 +0800

    feat:run first demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use  ferris_says::say;
+use  std::io::{stdout,BufWriter};
 fn main() {
-    println!("Hello, world!");
+    let std_out = stdout();
+    let message = String::from("Hello rust world");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(std_out.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
 }
